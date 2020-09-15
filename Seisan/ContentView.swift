1518fee58d3f9e5341009f5f41c8ae40fb9cae26
iLickeyPro <lickeymut@icloud.com>
2020-09-15 17:56:31 +0900

    Initial Commit

---
+++ Seisan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Seisan
+//
+//  Created by 野口 勇気 on 2020/09/15.
+//  Copyright © 2020 Yuki Noguchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
